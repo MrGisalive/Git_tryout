e3860b30f53ccdff53c4c9d188c3032720d1694c
MrGisalive <kiscsaszi1998@gmail.com>
2025-04-26 13:29:35 +0200

    frissítés

--- hello_again.py
+++ hello_again.py
@@ -1,5 +1,10 @@
+from dotenv import load_dotenv
+import os
 import requests
-API_KEY = "7d97772fd62af034b1f860765b089dcf"
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 city = "Budapest"
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
