c46c912be4093686598b4fe55eee51bec1b48292
MrGisalive <kiscsaszi1998@gmail.com>
2025-04-26 12:40:28 +0200

    frissítettem a hello_again.py-t

--- hello_again.py
+++ hello_again.py
@@ -1,2 +1,8 @@
 import requests
+API_KEY = "7d97772fd62af034b1f860765b089dcf"
+city = "Budapest"
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
+response = requests.get(url)
+print(response)
+print(response.json())
